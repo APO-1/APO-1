655491aeef917f7d451cd6e271e432ed2932d375
JuanDLP <juanjosedelapava1@gmail.com>
2023-04-10 12:22:17 -0500

    Adding the findUser

--- Semana10/src/ui/Main.java
+++ Semana10/src/ui/Main.java
@@ -1,48 +1,46 @@
-package ui; 
+package ui;
 
-import java.util.Scanner; 
+import java.util.Scanner;
 import model.SavingsSystem;
 
-public class Main{
+public class Main {
 
     private SavingsSystem controller;
-    private Scanner reader;  
+    private Scanner reader;
 
-    // constructor de la clase 
-    public Main(){
-        this.reader = new Scanner(System.in); 
+    // constructor de la clase
+    public Main() {
+        this.reader = new Scanner(System.in);
         controller = new SavingsSystem();
     }
 
-    public static void main(String[] args){
-        Main view = new Main(); // la llamada al constructor de la clase 
+    public static void main(String[] args) {
+        Main view = new Main(); // la llamada al constructor de la clase
 
-        int option = 0; 
+        int option = 0;
 
-        do{
-            view.menu(); 
-            option = view.validateIntegerInput(); 
+        do {
+            view.menu();
+            option = view.validateIntegerInput();
             view.executeOption(option);
 
-        }while(option != 0);
-
+        } while (option != 0);
 
         view.reader.close();
     }
 
-    public void menu(){
-        System.out.println("0. Salir"); 
-        System.out.println("1. Add User"); 
-        System.out.println("2. Add Saving"); 
-        System.out.println("3. List savings"); 
+    public void menu() {
+        System.out.println("0. Salir");
+        System.out.println("1. Add User");
+        System.out.println("2. Add Saving");
+        System.out.println("3. List savings");
 
     }
 
-
-    public void executeOption(int option){
+    public void executeOption(int option) {
         switch (option) {
             case 1:
-                initUser(); 
+                initUser();
                 System.out.println(controller);
                 break;
 
@@ -50,65 +48,66 @@ public class Main{
                 addSaving();
                 break;
 
-            case 3: 
+            case 3:
                 String msj = controller.listAllSavings();
                 System.out.println(msj);
                 break;
 
             case 0:
-                System.out.println("Exit."); 
-                break; 
+                System.out.println("Exit.");
+                break;
 
-            case -1: 
-                System.out.println("Invalid Option!!"); 
-                break; 
+            case -1:
+                System.out.println("Invalid Option!!");
+                break;
         }
     }
 
-    public int validateIntegerInput(){
-        int option = 0; 
-        if(reader.hasNextInt()){
-            option = reader.nextInt(); 
+    public int validateIntegerInput() {
+        int option = 0;
+        if (reader.hasNextInt()) {
+            option = reader.nextInt();
+        } else {
+            reader.nextLine();// limpiar el scanner
+            option = -1;
+            System.out.println("Ingrese un valor entero.");
         }
-        else{
-            reader.nextLine();// limpiar el scanner 
-            option = -1; 
-            System.out.println("Ingrese un valor entero."); 
-        }
-        return option; 
+        return option;
     }
 
-    public void viewUserName(){
+    public void viewUserName() {
         System.out.println(controller.getUser().getName());
     }
 
-    public void initUser(){
-        String userName = ""; 
-        String userId = ""; 
+    public void initUser() {
+        String userName = "";
+        String userId = "";
 
-        System.out.println("Type your name"); 
-        userName = reader.next(); 
-        System.out.println("Type your id"); 
-        userId = reader.next(); 
+        System.out.println("Type your name");
+        userName = reader.next();
+        System.out.println("Type your id");
+        userId = reader.next();
 
-        // llamada a una operación de control 
-        controller.addUser(userName, userId); 
+        // llamada a una operación de control
+        controller.addUser(userName, userId);
         viewUserName();
 
     }
 
-    public void addSaving(){
-        double costSaving; 
-        String nameSaving; 
+    public void addSaving() {
+        double costSaving;
+        String nameSaving;
         int category;
+        String userID;
 
         System.out.println("type cost saving");
         costSaving = reader.nextDouble();
 
         System.out.println("type name saving");
-        nameSaving = reader.next();
+        reader.nextLine();
+        nameSaving = reader.nextLine();
 
-        System.out.println("Type type of Saving:"); 
+        System.out.println("Type type of Saving:");
         System.out.println(" 1. for TRANSPORT");
         System.out.println(" 2. for ");
         System.out.println(" 3. for ");
@@ -117,10 +116,14 @@ public class Main{
 
         category = reader.nextInt();
 
-        // Esto es una dependencia de objetos --> esto no debería hacerse 
+        System.out.println("Please insert the User's ID");
+        reader.nextLine();
+        userID = reader.nextLine();
+
+        // Esto es una dependencia de objetos --> esto no debería hacerse
         /** Saving saving = new Saving(nameSaving, costSaving); */
 
-        String msg = controller.addSaving(nameSaving, costSaving, category);
+        String msg = controller.addSaving(nameSaving, costSaving, category, userID);
         System.out.println(msg);
     }
 
