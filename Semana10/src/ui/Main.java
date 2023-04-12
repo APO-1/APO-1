1ae4e2c5ce348dda8e2cddfea735597e599ee26a
Andres-Chamorro <chamorroricardo29@gmail.com>
2023-04-12 17:49:39 -0500

    fourt commit

--- Semana10/src/ui/Main.java
+++ Semana10/src/ui/Main.java
@@ -134,11 +134,10 @@ public class Main{
         String id = reader.next();
         int pos = searchUserById(id);
         if (pos != -1) {
-           String nombre = users[pos].getNombre();
-           String apellido = users[pos].getApellido();
-           System.out.println("La persona con ID " + idBuscar + " es " + nombre + " " + apellido);
+           String name = users[pos].getName();
+           System.out.println("La persona con ID " + id + " es " + name);
         } else {
-           System.out.println("No se encontro ninguna persona con ID " + idBuscar);
+           System.out.println("No se encontro ninguna persona con ID " + id);
         }
     }
 
